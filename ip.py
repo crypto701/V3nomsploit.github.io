300fc5e6e65131483bba71313e47bce156821837
crypto701 <56203120+crypto701@users.noreply.github.com>
2019-10-05 16:03:24 -0500

    Update ip.py

--- ip.py
+++ ip.py
@@ -2,7 +2,7 @@
 if (!empty($_SERVER['HTTP_CLIENT_IP']))
     {
       $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
-    }
+    } 
 elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
     {
       $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
